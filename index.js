bb5eed05442634241f214c5510895377f02bb18b
AnyAng8 <anykarin2804@gmail.com>
2025-06-10 12:21:13 +0200

    ejercicios variables bucles

---
+++ index.js
@@ -0,0 +1,37 @@
+let variableSinValor
+let booleano1 = false
+let booleano2 = true
+
+const PI = 3.14
+const TAU= 2*(PI)
+
+let contarHasta10_2 = 0;
+for (contarHasta10_2=0;contarHasta10_2 < 10; contarHasta10_2++) {
+    contarHasta10_2++;
+}
+
+
+
+ booleanoAnd= booleano1==booleano2
+let booleanoNot = !booleano1;
+ booleanoMix0= booleano1==booleano2
+incrementarDesp=2
+resultadoDesp = incrementarDesp++
+ incrementarAntes= 2
+resultadoAntes= ++incrementarAntes
+
+
+
+
+console.log(contarHasta10_2===10)
+
+
+let variableValorNumerico= 28
+const MiNombre= "Any"
+const MiNumeroFav= 8
+booleanoOr= booleano1==booleano2
+
+valorSuma= MiNumeroFav+variableValorNumerico
+valorResta= MiNumeroFav-variableValorNumerico
+valorDivision= MiNumeroFav/3
+valorMultiplicacion= MiNumeroFav*variableValorNumerico
